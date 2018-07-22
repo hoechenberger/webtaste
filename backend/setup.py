25fcdccb6b2079d5ec31e8252d0ac8dd55e1fb8e
Richard Höchenberger <richard.hoechenberger@gmail.com>
2018-07-22 19:17:58 +0200

    PKG: Update dependencies

--- backend/setup.py
+++ backend/setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from setuptools import setup
 
@@ -8,6 +7,6 @@ setup(
     packages=['webtaste_backend'],
     include_package_data=True,
     install_requires=[
-        'flask', 'flask_restplus'
+        'flask', 'flask_restplus', 'mysqlclient'
     ],
 )
